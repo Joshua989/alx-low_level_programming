f6d67805d455d7060fcdfbe60b67e6d7d13a712b
joshua989 <Josipoli0222@gmail.com>
2022-10-26 12:40:45 -0700

    ggccc

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+
+/**
+ * main - prints largest prime factor.
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	long int n, fp;
+
+	n = 612852475143;
+	for (fp = 2; fp <= n; fp++)
+	{
+		if (n % fp == 0)
+		{
+			n /= fp;
+			fp--;
+		}
+
+	}
+	printf("%ld\n", fp);
+	return (0);
+}
+
+Task 12. 101-print_number.c
+
+#include "main.h"
+
+/**
+ * print_number - prints an integer.
+ * @n: input integer.
+ * Return: no return.
+ */
+void print_number(int n)
+{
+	unsigned int m, d, count;
+
+	if (n < 0)
+	{
+		_putchar(45);
+		m = n * -1;
+	}
+	else
+	{
+		m = n;
+	}
+
+	d = m;
+	count = 1;
+
+	while (d > 9)
+	{
+		d /= 10;
+		count *= 10;
+	}
+
+	for (; count >= 1; count /= 10)
+	{
+		_putchar(((m / count) % 10) + 48);
+	}
+}
+
