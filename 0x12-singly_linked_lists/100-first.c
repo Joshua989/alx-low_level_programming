a69e7b5ed0400cb6a43f9431a7cf4501f8992e01
joshua989 <Josipoli0222@gmail.com>
2022-11-24 14:34:08 -0800

    gghg

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -10,23 +10,3 @@ void __attribute__ ((constructor)) bmain()
 	printf("You're beat! and yet, you must allow");
 	printf(",\nI bore my house upon my back!\n");
 }
-
-
-
-vi 101-hello_holberton.asm
-SECTION .data
-msg:	db "Hello, Holberton", 0
-fmt:	db "%s", 10, 0
-
-	SECTION .text
-	extern printf
-	global main
-main:
-	mov esi, msg
-	mov edi, fmt
-	mov eax, 0
-	call printf
-
-	mov eax, 0
-	ret
-
